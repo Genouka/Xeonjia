ffc310015d486f339aef096d764dec35a6fdd37d
DeepDaikon <deepdaikon@tuta.io>
2020-02-26 13:46:43 +0100

    Improve rules page

--- lib/src/screens/rules/rules_page.dart
+++ lib/src/screens/rules/rules_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:xeonjia/src/resources/global_variables.dart';
 import 'package:xeonjia/src/screens/rules/resources/rules_list.dart';
+import 'package:xeonjia/src/util/local_data_controller.dart';
 
 class RulesPage extends StatefulWidget {
   @override
@@ -24,37 +25,44 @@ class _RulesPageState extends State<RulesPage> {
           title: const Text('R U L E S'),
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-            child: Column(
-          children: <Widget>[
-            /*Center(
+        body: GestureDetector(
+          child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: SingleChildScrollView(
+                  child: Column(
+                children: <Widget>[
+                  /*Center(
                 child: Container(
                     padding: EdgeInsets.only(top: 35),
                     child: Image(
                         image: AssetImage(ruleList[_page]['image']),
                         fit: BoxFit.fitWidth,
                         width: MediaQuery.of(context).size.width * 2 / 3))),*/
-            Container(
-              color: Colors.lightBlue[500],
-              height: 40,
-              width: double.infinity,
-              child: Center(
-                child: Text(
-                  ruleList[_page]['title'],
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      color: Colors.white, fontSize: kTextFontSize),
-                ),
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.fromLTRB(20, 35, 20, 0),
-                child: Text(
-                  ruleList[_page]['text'],
-                  style: const TextStyle(fontSize: kTextFontSize - 2),
-                )),
-          ],
-        )),
+                  Container(
+                    color: Colors.lightBlue[500],
+                    height: 40,
+                    width: double.infinity,
+                    child: Center(
+                      child: Text(
+                        ruleList[_page]['title'],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Colors.white, fontSize: kTextFontSize),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      padding: const EdgeInsets.fromLTRB(20, 35, 20, 0),
+                      child: Text(
+                        ruleList[_page]['text'],
+                        style: const TextStyle(fontSize: kTextFontSize - 2),
+                      )),
+                ],
+              ))),
+          onHorizontalDragEnd: (DragEndDetails details) {
+            changePage(details.velocity.pixelsPerSecond.dx > 0 ? -1 : 1);
+          },
+        ),
         bottomNavigationBar: BottomAppBar(
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -64,22 +72,26 @@ class _RulesPageState extends State<RulesPage> {
                   Icon(Icons.keyboard_arrow_left),
                   Text('Back')
                 ]),
-                onPressed: _page > 0
-                    ? () => setState(() {
-                          --_page;
-                        })
-                    : null,
+                onPressed: _page > 0 ? () => changePage(-1) : null,
               ),
               MaterialButton(
                   child: Row(children: const [
                     Text('Next'),
                     Icon(Icons.keyboard_arrow_right)
                   ]),
-                  onPressed: _page < ruleList.length - 1
-                      ? () => setState(() {
-                            ++_page;
-                          })
-                      : null)
+                  onPressed:
+                      _page < ruleList.length - 1 ? () => changePage(1) : null)
             ])),
       );
+
+  void changePage(int diff) {
+    setState(() {
+      _page += diff;
+      if (_page < 0) {
+        _page = 0;
+      } else if (_page >= ruleList.length) {
+        _page = ruleList.length - 1;
+      }
+    });
+  }
 }
