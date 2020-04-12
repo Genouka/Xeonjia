fde69de1d4c2b3e4a34bc2b078043400d6b9cfd1
DeepDaikon <deepdaikon@tuta.io>
2020-04-12 21:27:10 +0200

    Bump version 1.2.0 (5)

--- lib/src/resources/global_variables.dart
+++ lib/src/resources/global_variables.dart
@@ -4,7 +4,7 @@ import 'package:xeonjia/src/util/settings.dart';
 import 'package:xeonjia/src/util/character_info.dart';
 
 // App version
-const String appVersion = '1.1.1';
+const String appVersion = '1.2.0';
 
 // App font size
 const double kTextFontSize = 20;
