902d1f10dd688cfe92f7bdcd8c3d882577301655
DeepDaikon <deepdaikon@tuta.io>
2020-08-09 12:09:21 +0200

    feat: add slippery property

--- lib/game/components/static/static.dart
+++ lib/game/components/static/static.dart
@@ -6,25 +6,36 @@ import 'package:xeonjia/models/direction.dart';
 
 // Static component
 class StaticComponent extends BasicComponent {
-  // If true: other components can walk on this, else: this is a solid component
+  // If true: other components slide on this
+  final bool _slippery;
+
+  // If true: other components can walk on this
   final bool _walkable;
   StaticComponent(tile, {bool walkable = false})
-      : _walkable = walkable,
+      : _slippery = (tile.properties['slippery'] ?? 'false') == 'true',
+        _walkable = walkable,
         super.fromTile(tile);
 
   @override
   Rect collisionRect(DynamicComponent otherComponent) {
+    if (_slippery) return null;
     if (!_walkable) return super.collisionRect(otherComponent);
     if (otherComponent.isFlying() != isFlying() ||
         otherComponent.wasStationary) {
       return null;
     }
+    // If the other component is going left or right
     if (otherComponent.direction.dx != 0) {
+      // If otherComponent.center > this.center -> do nothing
+      // Else return a rect with width = 1 at the left or right of this
       return otherComponent.direction.dx * (otherComponent.x - x) > 0
           ? null
           : Rect.fromLTWH(
               x + (otherComponent.direction.dx > 0 ? width : -1), y, 1, height);
     } else {
+      // (going up or down)
+      // If otherComponent.center > this.center -> do nothing
+      // Else return a rect with height = 1 at the top of bottom of this
       return otherComponent.direction.dy * (otherComponent.y - y) > 0
           ? null
           : Rect.fromLTWH(
