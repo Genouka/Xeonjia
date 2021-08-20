9522480312482675e8f29c6b65ffd346713f8c37
DeepDaikon <deepdaikon@tuta.io>
2021-08-20 11:14:50 +0200

    fix: show backpack in the homes

--- lib/game/widgets/backpack_button.dart
+++ lib/game/widgets/backpack_button.dart
@@ -6,26 +6,24 @@ import 'package:xeonjia/game/xeonjia_game.dart';
 class BackpackButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return game.map.disableMiniMap
-        ? Container()
-        : Positioned(
-            top: 6,
-            right: 56,
-            child: InkWell(
-              onTap: () => game.messageManager.active ||
-                      game.hasAction ||
-                      !game.playerOne.isStationary
-                  ? null
-                  : game.backpack(),
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-                height: 34,
-                decoration: BoxDecoration(
-                    color: Colors.grey[800].withOpacity(0.7),
-                    borderRadius: const BorderRadius.all(Radius.circular(30))),
-                child: const Icon(Icons.backpack_rounded, color: Colors.white),
-              ),
-            ),
-          );
+    return Positioned(
+      top: 6,
+      right: game.map.disableMiniMap ? 6 : 56,
+      child: InkWell(
+        onTap: () => game.messageManager.active ||
+                game.hasAction ||
+                !game.playerOne.isStationary
+            ? null
+            : game.backpack(),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+          height: 34,
+          decoration: BoxDecoration(
+              color: Colors.grey[800].withOpacity(0.7),
+              borderRadius: const BorderRadius.all(Radius.circular(30))),
+          child: const Icon(Icons.backpack_rounded, color: Colors.white),
+        ),
+      ),
+    );
   }
 }
