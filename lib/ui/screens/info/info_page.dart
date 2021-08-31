83cbc6321af5f3844cdc9163bc43873ce20ee046
DeepDaikon <deepdaikon@tuta.io>
2021-08-31 21:05:02 +0200

    version: 2.1.2 (10)

--- lib/ui/screens/info/info_page.dart
+++ lib/ui/screens/info/info_page.dart
@@ -18,7 +18,7 @@ class _InfoPageState extends State<InfoPage> {
       'icon': const Icon(Icons.change_history),
     },
     {
-      'title': 'Version: %s'.i18n.fill(['2.1.1']),
+      'title': 'Version: %s'.i18n.fill(['2.1.2']),
       'subtitle': 'App version'.i18n,
       'url': '',
       'icon': const Icon(Icons.looks_two),
