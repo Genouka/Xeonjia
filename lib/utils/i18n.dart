35d91caaf5bf5e5c6dc5b21f0847c0ca29caffea
deepdaikon <deepdaikon@tuta.io>
2022-10-08 14:28:24 +0200

    locale: rename zh-Hans and pt-BR

--- lib/utils/i18n.dart
+++ lib/utils/i18n.dart
@@ -73,8 +73,8 @@ extension Localization on String {
     for (final locale in supportedLocales) {
       var language = locale.languageCode;
       if (language == 'en') continue;
-      if (language == 'zh') language += '_Hans';
-      if (language == 'pt') language += '_BR';
+      if (language == 'zh') language += '-Hans';
+      if (language == 'pt') language += '-BR';
       for (final fileName in ['story', 'ui']) {
         _translations += await GettextImporter().fromAssetFile(
             locale.languageCode, 'locale/$language/LC_MESSAGES/$fileName.po');
