5ff1b1cf515daa1c1ad568a38b49add1b4cc443b
deepdaikon <deepdaikon@tuta.io>
2022-10-09 14:09:10 +0200

    fix: use the right sfx filename

--- lib/game/utils/sfx.dart
+++ lib/game/utils/sfx.dart
@@ -2,5 +2,5 @@
 enum Sfx { collision, damage, item, punch, snowball, explosion, dialog }
 
 extension SfxFileName on Sfx {
-  String get fileName => 'audio/sfx/$name.oga';
+  String get fileName => 'sfx/$name.oga';
 }
