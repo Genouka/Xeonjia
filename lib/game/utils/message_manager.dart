f2147b04be84e1bc0922ff911817863ca5d753bb
deepdaikon <deepdaikon@tuta.io>
2022-10-15 12:47:27 +0200

    fix: do not split messages on double quotes

--- lib/game/utils/message_manager.dart
+++ lib/game/utils/message_manager.dart
@@ -63,7 +63,7 @@ class MessageManager {
   /// Split message in sentences and group them
   List<Message> _splitMessage(Message message) {
     var strings = <String>[];
-    RegExp(r'([^.,?!"»…。！？“”]*[.,?!"»…。？！“”]*)\s*')
+    RegExp(r'([^.,?!…。！？]*[.,?!»…。？！]*)\s*')
         .allMatches(message.text)
         .forEach((m) {
       var match = m.group(0);
