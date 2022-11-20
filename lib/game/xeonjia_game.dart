d20d2e156b97df4bd3cecf74f22f4a419201d79b
deepdaikon <deepdaikon@tuta.io>
2022-11-20 18:31:05 +0100

    perf: pause and resume bgm each time the room is changed

--- lib/game/xeonjia_game.dart
+++ lib/game/xeonjia_game.dart
@@ -500,10 +500,21 @@ class XeonjiaGame extends FlameGame
       FlameAudio.bgm.stop();
       return;
     }
-    if (newBgm == currentBgm) return;
+    var currentMap = map.id;
+    if (newBgm == currentBgm) {
+      // Workaround to "fix" performance degradation caused by sound effects.
+      // How to reproduce: play a sfx (e.g. Sfx.dialog) multiple times in the
+      // same room (~50 times or more).
+      // With this workaround the performance returns to normal with each room
+      // change. Don't know why.
+      // AudioPool, clearing cache, and other solutions didn't work.
+      FlameAudio.bgm.pause().then((_) {
+        if (!paused && currentMap == map.id) FlameAudio.bgm.resume();
+      });
+      return;
+    }
     currentBgm = newBgm;
     FlameAudio.bgm.stop();
-    var currentMap = map.id;
     Future.delayed(const Duration(seconds: 1), () {
       if (!paused && currentMap == map.id) {
         FlameAudio.bgm.play('bgm/' + currentBgm! + '.oga');
