fe4265b6cf209f490f170a3aa169960c0e646b37
deepdaikon <deepdaikon@tuta.io>
2023-03-05 12:57:12 +0100

    style: remove white line

--- lib/game/xeonjia_game.dart
+++ lib/game/xeonjia_game.dart
@@ -7,7 +7,6 @@ import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
-
 import 'package:xeonjia/game/xeonjia.dart';
 
 /// Default component dimension
