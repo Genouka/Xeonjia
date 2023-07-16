1f6e2c812e8bf1b7a24bdc4bc697af4a708a5c07
deepdaikon <deepdaikon@tuta.io>
2023-07-16 11:31:36 +0200

    fix: minutes-played as symbol instead of procedure

--- lib/game/utils/event_manager.dart
+++ lib/game/utils/event_manager.dart
@@ -11,6 +11,8 @@ Environment setEnvironment(XeonjiaGame gameRef) {
   }
 
   env.defineSymbol(Sym('hero'), mainCharacter.name);
+  env.defineSymbol(Sym('minutes-played'),
+      (mainCharacter.minutesPlayed + gameRef.elapsed / 60).round());
 
   // Return [actor, value] for Cells that have a default actor
   // e.g. (move 2) and (move '(0 93))
@@ -451,7 +453,6 @@ Environment setEnvironment(XeonjiaGame gameRef) {
     }
     return #NONE;
   });
-  _('minutes-played', 0, (Cell? x) => mainCharacter.minutesPlayed);
   _(
     'get',
     1,
