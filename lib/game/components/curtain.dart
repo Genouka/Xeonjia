367b1dcd4787de9b4b3adcac2bf20574d73d1eac
deepdaikon <deepdaikon@tuta.io>
2023-08-20 08:54:09 +0200

    refactor: clean up CurtainComponent

--- lib/game/components/curtain.dart
+++ lib/game/components/curtain.dart
@@ -6,26 +6,16 @@ import 'package:xeonjia/game/xeonjia_game.dart';
 
 /// Black curtain shown while changing room
 class CurtainComponent extends PositionComponent with HasGameRef<XeonjiaGame> {
-  Paint get paint =>
-      Paint()..color = const Color(0xff000000).withOpacity(_opacity);
-
   double _opacity = 1;
   bool get done => _opacity == 0;
   void start() => _opacity = 1;
 
   @override
   void render(Canvas canvas) {
-    if (!done) canvas.drawRect(Rect.fromLTWH(0, 0, width, height), paint);
-  }
-
-  @override
-  void onGameResize(Vector2 size) {
-    if (done) return;
-    x = gameRef.camera.position.x;
-    y = gameRef.camera.position.y;
-    width = size.x;
-    height = size.y;
-    super.onGameResize(size);
+    if (!done) {
+      canvas.drawPaint(
+          Paint()..color = const Color(0xff000000).withOpacity(_opacity));
+    }
   }
 
   @override
