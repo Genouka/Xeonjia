453c035f74f42805500f3c3ce5f7718153b71306
deepdaikon <deepdaikon@tuta.io>
2023-10-13 20:37:43 +0200

    fix: add missing import

--- lib/game/xeonjia_game.dart
+++ lib/game/xeonjia_game.dart
@@ -5,6 +5,7 @@ import 'package:collection/collection.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
+import 'package:flame_audio/bgm.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:xeonjia/game/xeonjia.dart';
